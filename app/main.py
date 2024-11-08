5ea1b6f974478b18fc861fa7cc981f39a4d68155
EnoshMcCarthy <enoshh12@gmail.com>
2024-11-08 19:22:44 +0200

    architecture

--- app/main.py
+++ app/main.py
@@ -1,21 +1,4 @@
-from app.rabbit.channel import create_channel
-from app.settings.config import ENOSH_QUEUE
-
-
-def publish_message(message: str):
-    with create_channel() as channel:
-        # Declare the queue (idempotent - will only create if doesn't exist)
-        channel.queue_declare(queue=ENOSH_QUEUE, durable=True)
-
-        # Publish the message
-        channel.basic_publish(
-            exchange='',  # Empty exchange for direct queue publishing
-            routing_key=ENOSH_QUEUE,
-            body=message.encode()
-        )
-
-        print(f" [x] Sent message: {message}")
-
+from app.rabbit.publish import publish_message
 
 if __name__ == '__main__':
     publish_message("Hello enosh!")
---
+++ app/rabbit/publish.py
@@ -0,0 +1,17 @@
+from app.rabbit.channel import create_channel
+from app.settings.config import ENOSH_QUEUE
+
+
+def publish_message(message: str):
+    with create_channel() as channel:
+        # Declare the queue (idempotent - will only create if doesn't exist)
+        channel.queue_declare(queue=ENOSH_QUEUE, durable=True)
+
+        # Publish the message
+        channel.basic_publish(
+            exchange='',  # Empty exchange for direct queue publishing
+            routing_key=ENOSH_QUEUE,
+            body=message.encode()
+        )
+
+        print(f" [x] Sent message: {message}")
