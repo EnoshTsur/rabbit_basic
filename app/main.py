df11a4c5bf7dfe0cbbd94e5941ae52bae1ede7fd
EnoshMcCarthy <enoshh12@gmail.com>
2024-11-09 08:59:45 +0200

    topic

--- app/main.py
+++ app/main.py
@@ -1,33 +1,34 @@
 import sys
 from app.rabbit.channel import create_channel
 from app.rabbit.consume import consume_messages
-from app.settings.config import DIRECT_QUEUE, DIRECT_EXCHANGE, ROUTING_KEY
+from app.settings.config import TOPIC_EXCHANGE, LOG_INFO_ROUTING_KEY
 
 if __name__ == '__main__':
     try:
         with create_channel() as channel:
             # Declare the fanout exchange
             channel.exchange_declare(
-                exchange=DIRECT_EXCHANGE,  # Exchange name
-                exchange_type='direct',  # Type of exchange
+                exchange=TOPIC_EXCHANGE,  # Exchange name
+                exchange_type='topic',  # Type of exchange
                 durable=True  # Ensure exchange durability
             )
 
             # Declare a queue (let RabbitMQ generate a unique name)
-            channel.queue_declare(queue=DIRECT_QUEUE, durable=True)
+            result = channel.queue_declare(queue='', durable=True)
+            queue_name = result.method.queue
 
             # Bind the queue to the direct exchange
             channel.queue_bind(
-                exchange=DIRECT_EXCHANGE,
-                queue=DIRECT_QUEUE,
-                routing_key=ROUTING_KEY
+                exchange=TOPIC_EXCHANGE,
+                queue=queue_name,
+                routing_key=LOG_INFO_ROUTING_KEY
             )
 
-            print(f" [*] Waiting for logs in queue: {DIRECT_QUEUE}.")
+            print(f" [*] Waiting for logs in queue: {queue_name}.")
 
             # Consume messages from the queue
             channel.basic_consume(
-                queue=DIRECT_QUEUE,
+                queue=queue_name,
                 on_message_callback=consume_messages,
                 auto_ack=False  # Automatically acknowledge the message
             )
--- app/settings/config.py
+++ app/settings/config.py
@@ -11,7 +11,10 @@ DIRECT_QUEUE = 'direct_bind'
 
 #routing keys
 ROUTING_KEY = 'demonstrate'
+LOG_INFO_ROUTING_KEY = "log.info"
+LOG_ALL_ROUTING_KEY = "log.*"
 
 #exchange
 LOGS_EXCHANGE = 'logs'
 DIRECT_EXCHANGE = 'amq.direct'
+TOPIC_EXCHANGE = 'amq.topic'
